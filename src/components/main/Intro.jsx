599b9ade854db5e750060bd0a612870ff8fd4e09
admiral84 <nasreddinejaouadi@hotmail.com>
2022-01-18 20:06:27 +0100

    release 1

--- src/components/main/Intro.jsx
+++ src/components/main/Intro.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './intro.scss'
+import { init } from 'ityped'
 
 const Intro = () => {
+    const job=document.getElementById("job");
+    //const job1=document.getElementById("job1");
+    init(job, { showCursor: false, strings: ['Software engineer', 'Web Designer' ] });
+    //init(job1, { showCursor: false, strings: ['Web DEveloper' ] });
     return (
-        <div className='intro'>
+
+        <div className='intro' >
+            <div className="left">
+                <img src="./assets/man.png" alt="" />
+            </div>
            
-        </div>
+           </div>
     )
 }
 
